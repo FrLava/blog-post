c896e815fcd29e2a78b4b2897ecdee213f6e6ede
Franc <franc.lavaud@gmail.com>
2018-04-27 22:47:49 +0200

    commit setup du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  blogPost  = [
+
+    {
+      titlePost: 'Titre 1',
+      contentPost: 'contenu blabla',
+      loveItsPost: 2,
+      created_atPost: new Date()
+    },
+
+    {
+      titlePost: 'Titre 2',
+      contentPost: 'contenu blablablabla',
+      loveItsPost: -2,
+      created_atPost: new Date()
+    }
+  ]
 }
